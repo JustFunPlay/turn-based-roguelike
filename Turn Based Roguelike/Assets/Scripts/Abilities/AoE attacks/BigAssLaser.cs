3a000716ccdbca1edc6b4b18e3a89559ca620ec6
JustFunPlay <dinandmeijer01@gmail.com>
2024-06-03 15:33:31 +0200

    Ability folder cleanup

---
+++ Turn Based Roguelike/Assets/Scripts/Abilities/AoE attacks/BigAssLaser.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New orbital laserBeam", menuName = "ScriptableObjects/Abilities/AoE Attacks/Orbital Laser")]
+public class BigAssLaser : BaseAbility
+{
+    [Header("AbilityDetails")]
+    [SerializeField] private int numberOfHits = 5;
+    [SerializeField] private float damageScalingPerHit = 0.5f;
+    [SerializeField] private float delayBetweenHits = 0.25f;
+    [SerializeField] private float bonusCritRate;
+
+    [SerializeField] private GameObject laserBeam;
+
+    protected override IEnumerator TriggerAbilityEffects(CombatPositionData caster, CombatPositionData[] validTargets)
+    {
+        yield return new WaitForSeconds(delayToInitialEffect);
+
+        Vector3 laserPosition = new Vector3();
+        foreach (CombatPositionData positionData in validTargets)
+        {
+            laserPosition += positionData.standingPosition.position;
+        }
+        laserPosition.x /= validTargets.Length;
+        laserPosition.y = 0;
+        laserPosition.z /= validTargets.Length;
+
+        Instantiate(laserBeam, laserPosition, Quaternion.identity);
+
+        for (int i = 0; i < numberOfHits; i++)
+        {
+            yield return new WaitForSeconds(delayBetweenHits);
+            foreach (var target in validTargets)
+            {
+                float critroll = Random.Range(0f, 1f) + bonusCritRate + caster.character.CritRate;
+                target.character.TakeDamage(caster.character.Magic * damageScalingPerHit * (critroll >= 1 ? 2 : 1), DamageType.Magic, out _);
+                if (critroll >= 1)
+                    caster.character.OnCrit();
+            }
+        }
+        
+        yield return base.TriggerAbilityEffects(caster, validTargets);
+    }
+}
